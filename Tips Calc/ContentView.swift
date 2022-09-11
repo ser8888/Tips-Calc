0d913070b46baf7ca480e9f54ef7aea2f7f0fe42
Sergejs Tiselskis <slokas85@gmail.con>
2022-09-11 17:48:55 +0300

    Initial Commit

---
+++ Tips Calc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tips Calc
+//
+//  Created by John Doe on 11/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tips Calc/Tips_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tips_CalcApp.swift
+//  Tips Calc
+//
+//  Created by John Doe on 11/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tips_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
