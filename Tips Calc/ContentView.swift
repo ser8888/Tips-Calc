d1c44a05347acb65acec46580f075ae863531e37
Sergejs Tiselskis <slokas85@gmail.con>
2022-09-11 19:03:02 +0300

    the draft is ready. Code optimisation required and design as well.

--- Tips Calc/ContentView.swift
+++ Tips Calc/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var guestNumber = 2
+    @State private var tipPercentage = 0
+    
+    let tipPercentages = [0, 5, 10, 15, 20]
+    
+    var totalPerGuest: Double {
+        let guestNumber = Double(guestNumber + 2 )
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let billTotal = Double(checkAmount) ?? 0
+        let tipValue = billTotal / 100 * tipSelection
+        let grandTotal = billTotal + tipValue
+        let amountPerGuest = grandTotal / guestNumber
+        
+        return amountPerGuest
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .font(.largeTitle)
+                        .foregroundColor(.red)
+                        .foregroundStyle(.indigo)
+                    Picker("Number of guests", selection:
+                            $guestNumber) {
+                            ForEach(2..<100) {
+                            Text("\($0) guests")
+                        }
+                    }
+                }
+                Section(header: Text("Tips you want to leave in total?"))
+                {
+                    Picker("Tip %%", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    Text("\(totalPerGuest, specifier: "%.2f")")
+                        .bold()
+                        .font(.largeTitle)
+                }
+            }
+            .navigationTitle("Tips calculation")
+            .navigationBarTitleDisplayMode(.inline)
+            
+        }
     }
 }
 
